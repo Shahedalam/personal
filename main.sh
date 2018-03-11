e93ed341200072883fecffc6d1d4b096ed6ac293
MD Shahed Alam <Shahedalam@users.noreply.github.com>
2018-03-12 00:30:55 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 rm newscollect.jar
-https://www.dropbox.com/s/7omwpl4suesatea/newscollect.jar
+wget https://www.dropbox.com/s/7omwpl4suesatea/newscollect.jar
 sudo chmod 777 newscollect.jar
 nohup java -jar newscollect.jar &
