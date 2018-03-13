203b0744954366f9832b3b9146cc931cbf235e02
MD Shahed Alam <Shahedalam@users.noreply.github.com>
2018-03-13 23:45:58 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,3 @@
-sudo rm n1.jar
-sudo rm n2.jar
-sudo rm n3.jar
-sudo wget https://www.dropbox.com/s/4v9sfdn89bfqfop/n1.jar
-sudo wget https://www.dropbox.com/s/hfxwiciuipoq4tc/n2.jar
-sudo wget https://www.dropbox.com/s/7b56j0nso3n31gn/n3.jar
-sudo chmod 777 n1.jar
-sudo chmod 777 n2.jar
-sudo chmod 777 n3.jar
 sudo nohup java -jar n1.jar &
 sudo nohup java -jar n2.jar &
 sudo nohup java -jar n3.jar &
